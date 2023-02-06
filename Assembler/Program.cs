07402f6d241afd1f0b1daa8d62b70ff948e84730
Peter <peterson.montez9@gmail.com>
2023-02-06 16:53:44 -0300

    inicio do assembler, avanço no CPU

---
+++ Assembler/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Você precisa passar um parâmetro para o arquivo a ser montado.");
+    return;
+}
+
+var filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("O arquivo especifiado não existe.");
+    return;
+}
+
+StreamWriter writer = null;
+StreamReader reader = null;
+try
+{
+    writer = new StreamWriter("memory");
+    writer.WriteLine("v2.0 raw");
+    reader = new StreamReader(filePath);
+
+    while (!reader.EndOfStream)
+    {
+        string line = reader.ReadLine();
+        line = processLine(line);
+        writer.Write(line);
+        writer.Write(" ");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("O seguinte erro ocorreu durante o processo:");
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    reader.Close();
+    writer.Close();
+}
+
+string processLine(string line)
+{
+    byte[] opCode = new byte[16]
+    Console.WriteLine(opCode);
+
+    return toHex(opCode);
+}
+
+string toHex(byte[] code)
+{
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+
+    string hexString = BitConverter.ToString(code).Replace("-", string.Empty);
+    Console.WriteLine(hexString);
+    return hexString;
+}
