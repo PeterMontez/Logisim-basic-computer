e12b97c41844990489194406e8918edced98178b
Peter <peterson.montez9@gmail.com>
2023-02-06 23:47:38 -0300

    nao sei o que to fazendo mas ta indo pra algum lugar

--- Assembler/Program.cs
+++ Assembler/Program.cs
@@ -44,20 +44,84 @@ finally
 
 string processLine(string line)
 {
-    byte[] opCode = new byte[16]
-    Console.WriteLine(opCode);
+
+    byte[] opCode = new byte[16];
+
+    byte[] inst = new byte[4];
+    byte[] par0 = new byte[4];
+    byte[] par1 = new byte[4];
+    byte[] par2 = new byte[4];
+
+    string[] parts = line.Split(' ');
+
+    int index = 0;
+    
+    switch (parts[0]){
+        case "mov":
+            byte[] r = convertValue4bits(Convert.ToInt32(parts[1].Remove('$').Remove(',')));
+            byte[] c = convertValue8bits(Convert.ToInt32(parts[2]));
+            opCode = new byte[16] {0, 0, 1, 0, r[0], r[1], r[2], r[3], c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7]};
+            foreach (var item in opCode)
+            {
+                Console.WriteLine(item);
+            }    
+            break;
+        case "inc":
+            //Console.WriteLine("Increment register: " + parts[1]);
+            break;
+        case "cmp":
+            //Console.WriteLine("Compare registers: " + parts[1] + " and " + parts[2]);
+            break;
+        case "je":
+            //Console.WriteLine("Jump to label: " + parts[1]);
+            break;
+        case "jump":
+           // Console.WriteLine("Unconditional jump to label: " + parts[1]);
+            break;
+        case "end:":
+            //Console.WriteLine("End of program");
+            break;
+        default:
+          //  Console.WriteLine("Unknown instruction");
+            break;
+    }    
 
     return toHex(opCode);
 }
 
-string toHex(byte[] code)
+byte[] convertValue4bits(int value)
 {
-    for (int i = 0; i < length; i++)
+    byte[] binary = new byte[4]; 
+    for (int i = 0; i < 4; i++)
     {
-        
+        binary[3 - i] = (byte)((value & (1 << i)) >> i);
     }
 
-    string hexString = BitConverter.ToString(code).Replace("-", string.Empty);
-    Console.WriteLine(hexString);
-    return hexString;
+    return binary;
+}
+
+byte[] convertValue8bits(int value)
+{
+    byte[] binary = new byte[8]; 
+    for (int i = 0; i < 8; i++)
+    {
+        binary[7 - i] = (byte)((value & (1 << i)) >> i);
+    }
+
+    return binary;
+}
+
+
+string toHex(byte[] code)
+{
+    string hex = "";
+
+    for (int i = 0; i < code.Length; i += 4)
+    {
+        int value = code[i] << 3 | code[i + 1] << 2 | code[i + 2] << 1 | code[i + 3];
+        hex += value.ToString("X1");
+    }
+
+    Console.WriteLine(hex);
+    return hex;
 }
